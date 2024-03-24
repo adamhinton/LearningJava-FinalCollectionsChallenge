255962cdf28faf58fbdf50cadd4f2b8568812b41
adamd <adam.d.hinton@gmail.com>
2024-03-24 12:02:11 -0400

    Working solution ish

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,151 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+}
+
+class Store{
+
+    public static void main(String[] args) {
+
+    }
+
+    private Set<InventoryItem> inventory;
+
+    private HashMap<LocalDate, Cart> carts;
+
+    // Keyed by product category
+    // Not sure I did this right
+    private HashMap<String, InventoryItem> aisleInventory;
+
+
+
+
+    public void listProductsByCategory(){
+
+    }
+
+}
+
+enum ProductCategory{
+
+    PRODUCE, DAIRY, CEREAL, MEAT, BEVERAGE
+}
+
+record Product (String sku, String name, String mfgr, ProductCategory category){ }
+
+enum CartType{
+    PHYSICAL, VIRTUAL
+}
+
+class Cart {
+    public static int ID = 1;
+
+    private int id;
+
+    private Set<Product> products;
+
+    private LocalDate date;
+
+    private CartType type;
+
+    // Add item to cart
+    public void addItem (Product product){
+        if (products.contains(product)){
+            System.out.println("Product already in cart");
+            return;
+        }
+        else{
+            products.add(product);
         }
     }
+
+    public void removeItem(Product product){
+        products.remove(product);
+    }
+
+    // Flesh this out later
+    public void printSalesSlip(){
+
+    }
+
+    public Cart(Set<Product> products, LocalDate date) {
+        this.products = products;
+        this.date = LocalDate.now();
+        // Not sure I did this right
+        this.id = ID;
+        ID++;
+    }
+}
+
+
+
+class InventoryItem{
+
+    private Product product;
+    private int qtyTotal;
+    private int qtyReserved;
+    private int qtyReorder;
+    private int qtyLow;
+    private double salesPrice;
+
+    public InventoryItem(Product product, int qtyTotal, int qtyReorder, int qtyLow, double salesPrice) {
+        this.product = product;
+        this.qtyTotal = qtyTotal;
+        this.qtyReorder = qtyReorder;
+        this.qtyLow = qtyLow;
+        this.salesPrice = salesPrice;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int getQtyTotal() {
+        return qtyTotal;
+    }
+
+    public int getQtyReserved() {
+        return qtyReserved;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+
+    public void reserveItem(int numItems){
+        qtyReserved += numItems;
+    }
+
+    // Not sure what this is supposed to be
+    // Maybe release item from reservation?
+    public void releaseItem(int numItems) {
+        qtyReserved-= numItems;
+    }
+
+    public void sellItem(int numItems){
+
+        if (qtyTotal <= numItems){
+            System.out.println("Insufficient inventory");
+            return;
+        }
+
+        qtyTotal -= numItems;
+
+        if(qtyTotal <= qtyLow){
+            placeInventoryOrder();
+        }
+    }
+
+    public void placeInventoryOrder(){
+        qtyTotal += qtyReorder;
+    }
+
 }
