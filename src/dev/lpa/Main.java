ef3d638ac7f477e95d5e88ab91d1b3673d5baf87
adamd <adam.d.hinton@gmail.com>
2024-03-24 23:27:33 -0400

    more code cleanup

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,7 @@
 package dev.lpa;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,23 +12,58 @@ public class Main {
 class Store{
 
     public static void main(String[] args) {
+    }
+
+
+    private static Random random = new Random();
+
+
+
+    private Map<String, InventoryItem> inventory;
+
+    private NavigableSet<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
+
+    private Map<ProductCategory, Map<String, InventoryItem>> aisleInventory;
+
+
+    private void manageStoreCarts(){}
+
+    private boolean checkOutCart(Cart cart){
+        return false;
+    }
+
+    private void abandonCarts(){}
+
+    private void listProductsByCategory(){
 
     }
 
-    private Set<InventoryItem> inventory;
+    private void stockStore(){
+        inventory = new HashMap<>();
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("A100","apple","local",ProductCategory.PRODUCE),
+                new Product("B100","banana","local",ProductCategory.PRODUCE),
+                new Product("P100","pear","local",ProductCategory.PRODUCE),
+                new Product("L103","lemon","local",ProductCategory.PRODUCE),
+                new Product("M201","milk","farm",ProductCategory.DAIRY),
+                new Product("Y001","yogurt","farm",ProductCategory.DAIRY),
+                new Product("C333","cheese","farm",ProductCategory.DAIRY),
+                new Product("R777","rice chex","Nabisco",ProductCategory.CEREAL),
+                new Product("G111","granola","Nat Valley",ProductCategory.CEREAL),
+                new Product("BB11","ground beef","butcher",ProductCategory.MEAT),
+                new Product("CC11","chicken","butcher",ProductCategory.MEAT),
+                new Product("BC11","bacon","butcher",ProductCategory.MEAT),
+                new Product("BC77","coke","coca cola",ProductCategory.BEVERAGE),
+                new Product("BC88","coffee","value",ProductCategory.BEVERAGE),
+                new Product("BC99","tea","herbal",ProductCategory.BEVERAGE)
+        ));
 
-    private HashMap<LocalDate, Cart> carts;
+        products.forEach(p -> inventory.put(p.sku(), new InventoryItem(p, random.nextDouble(0, 1.25), 1000, 5)));
 
-    // Keyed by product category
-    // Not sure I did this right
-    private HashMap<String, InventoryItem> aisleInventory;
-
-
-
-
-    public void listProductsByCategory(){
 
     }
+
+    private void stockAIsles(){}
 
 }
 
@@ -62,15 +95,60 @@ class Cart {
 //     Add item to cart
     public void addItem (InventoryItem item, int qty){
         products.merge(item.getProduct().sku(), qty, Integer::sum);
+
+        if (!item.reserveItem(qty)){
+            System.out.println("Could not add item. You suck!");
+        }
     }
 
-    public void removeItem(Product product){
-        products.remove(product);
+    public void removeItem(InventoryItem item, int qty){
+        int current = products.get(item.getProduct().sku());
+
+        if(current <= qty){
+            qty = current;
+            products.remove(item.getProduct().sku());
+            System.out.printf("Item [%s] removed from basket %n", item.getProduct().name());
+        }
+        else{
+            products.merge(item.getProduct().sku(), qty, (oldVal, newVal) -> oldVal - newVal);
+            System.out.printf("%d [%s]s removed%n", qty, item.getProduct().name());
+        }
+        item.releaseItem(qty);
     }
+
+
 
     // Flesh this out later
-    public void printSalesSlip(){
+    public void printSalesSlip(Map<String, InventoryItem> inventory){
 
+        double total = 0;
+
+        System.out.println("-".repeat(30));
+        System.out.println("Thank you for your sale: ");
+
+        for(var cartItem : products.entrySet()){
+
+            var item = inventory.get(cartItem.getKey());
+            int qty = cartItem.getValue();
+            double itemizedPrice = (item.getPrice() * qty);
+            total += itemizedPrice;
+            System.out.printf("\t%s %-10s (%d)@ $%.2f = $%.2f%n", cartItem.getKey(), item.getProduct().name(), qty,
+                    item.getPrice(), itemizedPrice);
+        }
+
+        System.out.printf("Total Sale: $%.2f%n", total);
+        System.out.println("-".repeat(30));
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "id=" + id +
+                ", cartDate=" + cartDate +
+                ", products=" + products +
+                '}';
     }
 
     public Cart(CartType cartType, int days) {
@@ -108,7 +186,7 @@ class InventoryItem{
     private int qtyLow;
     private double price;
 
-    public InventoryItem(Product product, int qtyTotal, int qtyLow, double price) {
+    public InventoryItem(Product product, double price, int qtyTotal, int qtyLow) {
         this.product = product;
         this.qtyTotal = qtyTotal;
         this.qtyLow = qtyLow;
